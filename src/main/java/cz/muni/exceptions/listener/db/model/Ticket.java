a86c13a51e845f1a4b689e70de94da9dddafd0bd
Jan Ferko <julyloov@gmail.com>
2014-04-15 18:37:31 +0200

    Added database model

---
+++ src/main/java/cz/muni/exceptions/listener/db/model/Ticket.java
@@ -0,0 +1,111 @@
+package cz.muni.exceptions.listener.db.model;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author Jan Ferko
+ * @sa.date 2014-04-15T05:40:59+0100
+ */
+@Entity
+public class Ticket implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String detailMessage;
+    
+    private String stackTrace;
+    
+    private int ticketClassId;
+    
+    @OneToMany
+    @JoinColumn(name = "ticket_id", referencedColumnName = "id")
+    private List<TicketOccurence> occurences;
+
+    public Ticket() {
+    }
+
+    public Ticket(String detailMessage, String stackTrace, 
+            TicketClass ticketClass, List<TicketOccurence> occurences) {        
+        this.detailMessage = detailMessage;
+        this.stackTrace = stackTrace;        
+        this.occurences = occurences;
+        this.ticketClassId = ticketClass == null 
+                ? TicketClass.UNKNOWN.getId() : ticketClass.getId();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDetailMessage() {
+        return detailMessage;
+    }
+
+    public void setDetailMessage(String detailMessage) {
+        this.detailMessage = detailMessage;
+    }
+
+    public String getStackTrace() {
+        return stackTrace;
+    }
+
+    public void setStackTrace(String stackTrace) {
+        this.stackTrace = stackTrace;
+    }
+
+    public List<TicketOccurence> getOccurences() {
+        return occurences;
+    }
+
+    public void setOccurences(List<TicketOccurence> occurences) {
+        this.occurences = occurences;
+    }
+    
+    public TicketClass getTicketClass() {
+        return TicketClass.find(ticketClassId);
+    }
+    
+    public void setTicketClass(TicketClass ticketClass) {
+        this.ticketClassId = ticketClass == null 
+                ? TicketClass.UNKNOWN.getId() : ticketClass.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Ticket)) {
+            return false;
+        }
+        final Ticket other = (Ticket) obj;
+        return Objects.equals(this.id, other.id);
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("Ticket {id=%1$s, detailMessage=%2$s, class=%3$s}", 
+                id, detailMessage, getTicketClass());
+    }
+
+}
