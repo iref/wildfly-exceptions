c0f305d42049cc30bf56f5780d9e4799a8c6e26a
Jan Ferko <julyloov@gmail.com>
2014-05-22 04:33:51 +0200

    Removed schema name from database schema \

--- src/main/resources/sql/database-drop.sql
+++ src/main/resources/sql/database-drop.sql
@@ -1,3 +1,2 @@
-DROP TABLE exceptions.ticket_occurrences if EXISTS;
-DROP TABLE exceptions.tickets IF EXISTS;
-DROP SCHEMA exceptions IF EXISTS;
+DROP TABLE ticket_occurrences if EXISTS;
+DROP TABLE tickets IF EXISTS;
